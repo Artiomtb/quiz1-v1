7e679bc6e0bf70649f22b1acc90f40a44e72831a
Curselolz <alex.kononenko@yahoo.com>
2017-02-03 17:26:57 +0200

    create(quiz):Create Renderable

--- PostCreator.php
+++ PostCreator.php
@@ -5,28 +5,41 @@
  * Date: 01.02.17
  * Time: 19:57
  */
+use Exceptions\ClassNotFoundException;
+use Exceptions\InvalidPostKeyException;
 
-class PostCreator {
+class PostCreator
+{
 
-  public function  __construct() {
+  public function  __construct()
+  {
+
+  }
+
+  private function  __clone()
+  {
+
   }
 
     private static $instance = null;
 
-    public function getInstance() {
+    static function getInstance()
+    {
         if (null === self::$instance) {
             self::$instance = new self();
         }
         return self::$instance;
     }
 
-    public function make(string $className,array $options):Model\Renderable {
+    static function make(string $className,array $options):Model\Renderable
+    {
         $namespacePaths = include 'config.php';
-        if(array_key_exists($className,$namespacePaths)){
-            if(class_exists($namespacePaths[className])) {
-                return new $namespacePaths[className]($options);
+
+        if(array_key_exists($className, $namespacePaths)){
+            if(class_exists($namespacePaths[$className])) {
+                return new $namespacePaths[$className]($options);
             } else {
-                throw new ClassNotFoundException;
+                throw new ClassNotFoundException();
             }
         } else {
             throw new InvalidPostKeyException();
